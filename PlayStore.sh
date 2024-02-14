70feff83ef0ea1e869f1a7a6e75b1cf219aed048
Allllllllllll <137359388+Alizaaaja4@users.noreply.github.com>
2024-02-14 12:01:55 +0700

    finish program allhamdullihah

--- PlayStore.sh
+++ PlayStore.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-
 login_admin(){
     data_use="linux"
     data_pass="2024"
@@ -133,23 +132,190 @@ show_admin(){
     fi
 }
 
+search_apk_player(){
+    echo "================================="
+    echo "||       Search Aplikasi       ||"
+    echo "================================="
+
+    cat daftar_apk.txt
+
+    echo ""
+    read -p "press to continue..."
+    clear
+
+    show_player
+}
+
+Install_apk_player(){
+    echo "================================="
+    echo "||      Install Aplikasi       ||"
+    echo "================================="
+
+    cat daftar_apk.txt
+
+    echo ""
+    echo "Nomor baris yang ingin ada install: "
+    read install
+
+    sed -n "${install}p" daftar_apk.txt >> file_unduh.txt
+
+    echo ""
+    echo "aplikasi berhasil di install"
+    read -p "press to continue..."
+    clear
+
+    show_player    
+}
+
+uninstall_apk_player(){
+    echo "================================="
+    echo "||     Uninstall Aplikasi      ||"
+    echo "================================="
+
+    cat file_unduh.txt
+
+    echo ""
+    echo -n "Uninstall line (1-999): "
+    read uninstall
+
+    sed -i "${uninstall}d" file_unduh.txt
+
+    echo ""
+    echo "Aplikasi berhasil di uninstall!!"
+    read -p "press to continue..."
+    clear
+
+    show_player
+}
+
+view_apk_player(){
+    echo "================================="
+    echo "||         View Player         ||"
+    echo "================================="
+
+    cat file_unduh.txt
+
+    echo ""
+    read -p "Press to continue..."
+    clear
+
+    show_player
+}
+
+show_player(){
+    echo "================================="
+    echo "||        Welcome Player       ||"
+    echo "================================="
+    echo "|| [1]. Search Aplikasi        ||"
+    echo "|| [2]. Install Aplikasi       ||"
+    echo "|| [3]. Uninstall Aplikasi     ||"
+    echo "|| [4]. View Aplikasi          ||"
+    echo "|| [5]. Logout                 ||"
+    echo "================================="
+    echo -n "/> "
+    read option
+
+    if [ "$option" -eq 1 ]; then
+        read -p "press to continue..."
+        clear
+        search_apk_player
+    
+    elif [ "$option" -eq 2 ]; then
+        read -p "press to continue..."
+        clear
+        Install_apk_player
+    
+    elif [ "$option" -eq 3 ]; then
+        read -p "press to continue..."
+        clear
+        uninstall_apk_player
+
+    elif [ "$option" -eq 4 ]; then
+        read -p "press to continue..."
+        clear
+        view_apk_player
+
+    elif [ "$option" -eq 5 ]; then
+        read -p "press to continue..."
+        clear
+        show_menu
+    
+    else
+        echo "option tidak valid"
+        echo -p "press to continue..."
+        clear
+
+        show_player
+    fi 
+}
+
+signIn_player(){
+    player_user="player"
+    player_email="allUbuntu@gmail.com"
+    player_pass="000"
+    
+    echo "================================="
+    echo "||        Sign In Player       ||"
+    echo "================================="
+    echo -n "Username: "
+    read user_player
+
+    echo ""
+    echo -n "Email: "
+    read email_player
+
+    echo ""
+    echo -n "Password: "
+    read pass_player
+
+    if [[ $user_player == $player_user && $email_player == $player_email && $pass_player == $player_pass ]]; then    
+        echo ""
+        echo "Akses diterima!!"
+        read -p "press to continue..."
+        clear
+        show_player
+    
+    else
+        echo ""
+        echo "Akses ditolak"
+        read -p "press to continue..."
+        clear
+        show_menu
+    
+    fi
+}
+
 show_menu(){
     echo "================================="
-    echo "||      Ubuntu Store Linux     ||"
+    echo "||       PlayStore Linux       ||"
     echo "================================="
     echo "|| [1]. Administrator          ||"
     echo "|| [2]. Customer               ||"
+    echo "|| [3]. Logout                 ||"
     echo "================================="
     echo -n "/> "
 
     read option
-    if [ $option -eq 1 ]
-    then 
+    if [ "$option" -eq 1 ]; then 
         read -p "press to continue..."
         clear
         login_admin
+    
+    elif [ "$option" -eq 2 ]; then
+        read -p "press to continue..."
+        clear
+        signIn_player
+    
+    elif [ "$option" -eq 3 ]; then
+        read -p "press to continue..."
+        clear
+        echo "Logout Success!!!"
+
     else 
-        echo 'hallo user'
+        read -p "Maaf perintah tidak valid..."
+        clear
+        show_menu
+        
     fi
 }
 
