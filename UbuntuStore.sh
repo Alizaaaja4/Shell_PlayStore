6a69b14bbf08ce4c14fa036994edf5cac2ffe1c3
Allllllllllll <137359388+Alizaaaja4@users.noreply.github.com>
2024-02-14 07:25:53 +0700

    finish fitur admin

--- UbuntuStore.sh
+++ UbuntuStore.sh
@@ -29,15 +29,108 @@ login_admin(){
     fi
 }
 
+add_aplikasi(){
+    echo "================================="
+    echo "||        Add Aplikasi         ||"
+    echo "================================="
+    echo "Nama Aplikasi: "
+    read nama_apk
+
+    echo ""
+    echo "Versi Aplikasi: "
+    read versi_apk
+
+    echo ""
+    echo "Deksripsi Aplikasi: "
+    read deks_apk
+
+    echo ""
+    echo "IDR?"
+    read idr_apk
+
+    echo "$nama_apk | $versi_apk | $deks_apk | $idr_apk" >> daftar_apk.txt
+
+    echo ""
+    read -p "loading..."
+
+    echo ""
+    echo "Aplikasi berhasil ditambahkan!!"
+
+    read -p "press to continue..."
+    clear
+    show_admin
+}
+
+list_aplikasi(){
+    echo "================================="
+    echo "||       Daftar Aplikasi       ||"
+    echo "================================="
+
+    cat daftar_apk.txt
+
+    echo ""
+    read -p "Press to continue..."
+    clear
+
+    show_admin
+}
+
+delate_aplikasi(){
+    echo "================================="
+    echo "||       Delete Aplikasi        ||"
+    echo "================================="
+
+    echo -n "Hapus baris line (1-999): "
+    read delete_line
+
+    sed -i "${delete_line}d" daftar_apk.txt
+
+    echo ""  
+    echo "Aplikasi berhasil dihapus"
+    read -p "Press to continue..."
+    clear  
+
+    show_admin
+}
+
 show_admin(){
     echo "================================="
     echo "||        Welcome Admin        ||"
     echo "================================="
     echo "|| [1]. Add Aplikasi           ||"
     echo "|| [2]. List Aplikasi          ||"
-    echo "|| [3]. Delate Aplikasi        ||"
+    echo "|| [3]. Delete Aplikasi        ||"
     echo "|| [4]. Logout                 ||"
     echo "================================="
+    echo -n "/> "
+    read option
+
+    if [ "$option" -eq 1 ]; then
+        read -p "press to continue..."
+        clear
+        add_aplikasi
+
+    elif [ "$option" -eq 2 ]; then
+        read -p "press to continue..."
+        clear
+        list_aplikasi
+
+    elif [ "$option" -eq 3 ]; then
+        read -p "press to continue..."
+        clear
+        delate_aplikasi
+    
+    elif [ "$option" -eq 4 ]; then
+        read -p "press to continue..."
+        clear
+        show_menu
+
+    else
+        read -p "Maaf perintah tidak valid..."
+        clear
+        show_admin
+
+    fi
 }
 
 show_menu(){
